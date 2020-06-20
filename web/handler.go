ea4cd56c8547999b6295eef48590e0df82d3d1e0
nahuakang <kangnahua@gmail.com>
2020-06-20 21:05:40 +0200

    Implement PostsStore, PostsShow methods

--- web/handler.go
+++ web/handler.go
@@ -76,9 +76,34 @@ func (h *Handler) ThreadsCreate() http.HandlerFunc {
 
 // ThreadsShow shows all the threads
 func (h *Handler) ThreadsShow() http.HandlerFunc {
+	type data struct {
+		Thread goreddit.Thread
+		Posts  []goreddit.Post
+	}
+
 	tmpl := template.Must(template.ParseFiles("templates/layout.html", "templates/thread.html"))
 	return func(w http.ResponseWriter, r *http.Request) {
-		tmpl.Execute(w, nil)
+		idStr := chi.URLParam(r, "id")
+		// convert idStr to UUID first
+		id, err := uuid.Parse(idStr)
+		if err != nil {
+			http.NotFound(w, r)
+			return
+		}
+
+		t, err := h.store.Thread(id)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		pp, err := h.store.PostsByThread(t.ID)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		tmpl.Execute(w, data{Thread: t, Posts: pp})
 	}
 }
 
@@ -123,9 +148,25 @@ func (h *Handler) ThreadsDelete() http.HandlerFunc {
 
 // PostsCreate leads to the page for creating new post
 func (h *Handler) PostsCreate() http.HandlerFunc {
+	type data struct {
+		Thread goreddit.Thread
+	}
+
 	tmpl := template.Must(template.ParseFiles("templates/layout.html", "templates/post_create.html"))
 	return func(w http.ResponseWriter, r *http.Request) {
-		tmpl.Execute(w, nil)
+		idStr := chi.URLParam(r, "id")
+		id, err := uuid.Parse(idStr)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		t, err := h.store.Thread(id)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		tmpl.Execute(w, data{Thread: t})
 	}
 }
 
@@ -133,25 +174,71 @@ func (h *Handler) PostsCreate() http.HandlerFunc {
 func (h *Handler) PostsStore() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		title := r.FormValue("title")
-		description := r.FormValue("description")
+		content := r.FormValue("content")
 
-		if err := h.store.CreateThread(&goreddit.Thread{
-			ID:          uuid.New(),
-			Title:       title,
-			Description: description,
-		}); err != nil {
+		idStr := chi.URLParam(r, "id")
+
+		id, err := uuid.Parse(idStr)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		t, err := h.store.Thread(id)
+		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 
-		http.Redirect(w, r, "/threads", http.StatusFound)
+		p := &goreddit.Post{
+			ID:       uuid.New(),
+			ThreadID: t.ID,
+			Title:    title,
+			Content:  content,
+		}
+
+		if err := h.store.CreatePost(p); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		http.Redirect(w, r, "/threads/"+t.ID.String()+"/"+p.ID.String(), http.StatusFound)
 	}
 }
 
 // PostsShow leads to the page for creating new post
 func (h *Handler) PostsShow() http.HandlerFunc {
+	type data struct {
+		Thread goreddit.Thread
+		Post   goreddit.Post
+	}
 	tmpl := template.Must(template.ParseFiles("templates/layout.html", "templates/post.html"))
 	return func(w http.ResponseWriter, r *http.Request) {
-		tmpl.Execute(w, nil)
+		postIDStr := chi.URLParam(r, "postID")
+		threadIDStr := chi.URLParam(r, "threadID")
+
+		postID, err := uuid.Parse(postIDStr)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		threadID, err := uuid.Parse(threadIDStr)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		p, err := h.store.Post(postID)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+
+		t, err := h.store.Thread(threadID)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+
+		tmpl.Execute(w, data{Thread: t, Post: p})
 	}
 }
