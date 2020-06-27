ba0d6889a944bc894075b5b6dd081ffa205c3981
nahuakang <kangnahua@gmail.com>
2020-06-27 20:09:23 +0200

    Refactor web handlers to thread, post, comment handlers

--- web/handler.go
+++ web/handler.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
-	"github.com/google/uuid"
 	"github.com/nahuakang/goreddit"
 )
 
@@ -17,21 +16,25 @@ func NewHandler(store goreddit.Store) *Handler {
 		store: store,
 	}
 
+	threads := ThreadHandler{store: store}
+	posts := PostHandler{store: store}
+	comments := CommentHandler{store: store}
+
 	h.Use(middleware.Logger)
 	h.Get("/", h.Home())
 	h.Route("/threads", func(r chi.Router) {
-		r.Get("/", h.ThreadsList())
-		r.Get("/new", h.ThreadsCreate())
-		r.Post("/", h.ThreadsStore())
-		r.Get("/{id}", h.ThreadsShow())
-		r.Post("/{id}/delete", h.ThreadsDelete())
-		r.Get("/{id}/new", h.PostsCreate())
-		r.Post("/{id}", h.PostsStore())
-		r.Get("/{threadID}/{postID}", h.PostsShow())
-		r.Get("/{threadID}/{postID}/vote", h.PostsVote())
-		r.Post("/{threadID}/{postID}", h.CommentsStore())
+		r.Get("/", threads.List())
+		r.Get("/new", threads.Create())
+		r.Post("/", threads.Store())
+		r.Get("/{id}", threads.Show())
+		r.Post("/{id}/delete", threads.Delete())
+		r.Get("/{id}/new", posts.Create())
+		r.Post("/{id}", posts.Store())
+		r.Get("/{threadID}/{postID}", posts.Show())
+		r.Get("/{threadID}/{postID}/vote", posts.Vote())
+		r.Post("/{threadID}/{postID}", comments.Store())
 	})
-	h.Get("/comments/{id}/vote", h.CommentsVote())
+	h.Get("/comments/{id}/vote", comments.Vote())
 
 	return h
 }
@@ -56,298 +59,5 @@ func (h *Handler) Home() http.HandlerFunc {
 			return
 		}
 		tmpl.Execute(w, data{Posts: pp})
-	}
-}
-
-// ThreadsList returns a webpage with the list of all Threads
-func (h *Handler) ThreadsList() http.HandlerFunc {
-	type data struct {
-		Threads []goreddit.Thread
-	}
-
-	tmpl := template.Must(template.ParseFiles("templates/layout.html", "templates/threads.html"))
-	return func(w http.ResponseWriter, r *http.Request) {
-		tt, err := h.store.Threads()
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		tmpl.Execute(w, data{Threads: tt})
-	}
-}
-
-// ThreadsCreate leads to the page for creating new threads
-func (h *Handler) ThreadsCreate() http.HandlerFunc {
-	tmpl := template.Must(template.ParseFiles("templates/layout.html", "templates/thread_create.html"))
-	return func(w http.ResponseWriter, r *http.Request) {
-		tmpl.Execute(w, nil)
-	}
-}
-
-// ThreadsShow shows all the threads
-func (h *Handler) ThreadsShow() http.HandlerFunc {
-	type data struct {
-		Thread goreddit.Thread
-		Posts  []goreddit.Post
-	}
-
-	tmpl := template.Must(template.ParseFiles("templates/layout.html", "templates/thread.html"))
-	return func(w http.ResponseWriter, r *http.Request) {
-		idStr := chi.URLParam(r, "id")
-		// convert idStr to UUID first
-		id, err := uuid.Parse(idStr)
-		if err != nil {
-			http.NotFound(w, r)
-			return
-		}
-
-		t, err := h.store.Thread(id)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		pp, err := h.store.PostsByThread(t.ID)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		tmpl.Execute(w, data{Thread: t, Posts: pp})
-	}
-}
-
-// ThreadsStore saves the newly created thread to database
-func (h *Handler) ThreadsStore() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		title := r.FormValue("title")
-		description := r.FormValue("description")
-
-		if err := h.store.CreateThread(&goreddit.Thread{
-			ID:          uuid.New(),
-			Title:       title,
-			Description: description,
-		}); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		http.Redirect(w, r, "/threads", http.StatusFound)
-	}
-}
-
-// ThreadsDelete deletes a thread based on its id
-func (h *Handler) ThreadsDelete() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		idStr := chi.URLParam(r, "id")
-
-		id, err := uuid.Parse(idStr)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
-		}
-
-		if err := h.store.DeleteThread(id); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		http.Redirect(w, r, "/threads", http.StatusFound)
-	}
-}
-
-// PostsCreate leads to the page for creating new post
-func (h *Handler) PostsCreate() http.HandlerFunc {
-	type data struct {
-		Thread goreddit.Thread
-	}
-
-	tmpl := template.Must(template.ParseFiles("templates/layout.html", "templates/post_create.html"))
-	return func(w http.ResponseWriter, r *http.Request) {
-		idStr := chi.URLParam(r, "id")
-		id, err := uuid.Parse(idStr)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
-		}
-
-		t, err := h.store.Thread(id)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		tmpl.Execute(w, data{Thread: t})
-	}
-}
-
-// PostsStore saves the newly created post to database
-func (h *Handler) PostsStore() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		title := r.FormValue("title")
-		content := r.FormValue("content")
-
-		idStr := chi.URLParam(r, "id")
-
-		id, err := uuid.Parse(idStr)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
-		}
-
-		t, err := h.store.Thread(id)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		p := &goreddit.Post{
-			ID:       uuid.New(),
-			ThreadID: t.ID,
-			Title:    title,
-			Content:  content,
-		}
-
-		if err := h.store.CreatePost(p); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		http.Redirect(w, r, "/threads/"+t.ID.String()+"/"+p.ID.String(), http.StatusFound)
-	}
-}
-
-// PostsShow leads to the page for creating new post
-func (h *Handler) PostsShow() http.HandlerFunc {
-	type data struct {
-		Thread   goreddit.Thread
-		Post     goreddit.Post
-		Comments []goreddit.Comment
-	}
-	tmpl := template.Must(template.ParseFiles("templates/layout.html", "templates/post.html"))
-	return func(w http.ResponseWriter, r *http.Request) {
-		postIDStr := chi.URLParam(r, "postID")
-		threadIDStr := chi.URLParam(r, "threadID")
-
-		postID, err := uuid.Parse(postIDStr)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
-		}
-
-		threadID, err := uuid.Parse(threadIDStr)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
-		}
-
-		p, err := h.store.Post(postID)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-		}
-
-		cc, err := h.store.CommentsByPost(p.ID)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-		}
-
-		t, err := h.store.Thread(threadID)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-		}
-
-		tmpl.Execute(w, data{Thread: t, Post: p, Comments: cc})
-	}
-}
-
-// CommentsStore saves the newly created comments of a post to database
-func (h *Handler) CommentsStore() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		content := r.FormValue("content")
-
-		idStr := chi.URLParam(r, "postID")
-
-		id, err := uuid.Parse(idStr)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
-		}
-
-		if err := h.store.CreateComment(&goreddit.Comment{
-			ID:      uuid.New(),
-			PostID:  id,
-			Content: content,
-		}); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		http.Redirect(w, r, r.Referer(), http.StatusFound)
-	}
-}
-
-// PostsVote stores information about votes on a post
-func (h *Handler) PostsVote() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		idStr := chi.URLParam(r, "postID")
-
-		id, err := uuid.Parse(idStr)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
-		}
-
-		p, err := h.store.Post(id)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
-		}
-
-		dir := r.URL.Query().Get("dir")
-		if dir == "up" {
-			p.Votes++
-		} else if dir == "down" {
-			p.Votes--
-		}
-
-		if err := h.store.UpdatePost(&p); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		http.Redirect(w, r, r.Referer(), http.StatusFound)
-	}
-}
-
-// CommentsVote stores information about votes on a comment
-func (h *Handler) CommentsVote() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		idStr := chi.URLParam(r, "id")
-
-		id, err := uuid.Parse(idStr)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
-		}
-
-		c, err := h.store.Comment(id)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
-		}
-
-		dir := r.URL.Query().Get("dir")
-		if dir == "up" {
-			c.Votes++
-		} else if dir == "down" {
-			c.Votes--
-		}
-
-		if err := h.store.UpdateComment(&c); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		http.Redirect(w, r, r.Referer(), http.StatusFound)
 	}
 }
