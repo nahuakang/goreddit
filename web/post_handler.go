ecd6bc8709d865af52f4e2e4db3732fd854904b1
nahuakang <kangnahua@gmail.com>
2020-06-30 21:43:54 +0200

    Implement form validate for PostHandler.Store method

--- web/post_handler.go
+++ web/post_handler.go
@@ -50,8 +50,15 @@ func (h *PostHandler) Create() http.HandlerFunc {
 // Store saves the newly created post to database
 func (h *PostHandler) Store() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		title := r.FormValue("title")
-		content := r.FormValue("content")
+		form := CreatePostForm{
+			Title:   r.FormValue("title"),
+			Content: r.FormValue("content"),
+		}
+		if !form.Validate() {
+			h.sessions.Put(r.Context(), "form", form)
+			http.Redirect(w, r, r.Referer(), http.StatusFound)
+			return
+		}
 
 		idStr := chi.URLParam(r, "id")
 
@@ -70,8 +77,8 @@ func (h *PostHandler) Store() http.HandlerFunc {
 		p := &goreddit.Post{
 			ID:       uuid.New(),
 			ThreadID: t.ID,
-			Title:    title,
-			Content:  content,
+			Title:    form.Title,
+			Content:  form.Content,
 		}
 
 		if err := h.store.CreatePost(p); err != nil {
